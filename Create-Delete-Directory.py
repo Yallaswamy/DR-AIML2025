b2010ced689d511df021a6b2fc1cc1409b949dfa
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2023-07-24 12:05:47 +0530

    Add files via upload

---
+++ Create-Delete-Directory.py
@@ -0,0 +1,11 @@
+import os
+#To Know about File Names and Floder Names
+#print(os.listdir(r"D:\AIML-CODING"))
+#TO Create the Directory in Any Floder
+#os.mkdir(r"D:\AIML-CODING\sample")
+#To Delete The Empty Directory Any Floder
+#os.rmdir(r"D:\AIML-CODING\sample")
+#to Delete The file in Any Directory and Any File
+#os.remove(r"D:\AIML-CODING\sample\hii.txt")
+
+
